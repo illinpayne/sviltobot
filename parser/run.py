b0149422b1d928fb384372d3930a620bafb895db
sachok11 <Olhasachko2711@gmail.com>
2025-12-09 16:01:08 +0200

    Fixed saving data by current datetime

--- parser/run.py
+++ parser/run.py
@@ -1,6 +1,4 @@
 import asyncio
-
-from parser.strategies.odesa import OdesaParser
 from parser.strategies.rivne import RivneParser
 from parser.core.models import ParseConfig
 from parser.scheduler.scheduler import run_periodically
@@ -14,15 +12,164 @@ parsers = [
         ),
         RivneParser()
     ),
-# (
-#         ParseConfig(
-#             region_name="Одеська область",
-#             url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622336/",
-#             save_path="data/odesa.json"
-#         ),
-#         OdesaParser()
-#     ),
-    #аналогічно інші області
+    # (
+    #     ParseConfig(
+    #         region_name="Одеська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622336/",
+    #         save_path="data/odesa.json"
+    #     ),
+    #     parser
+    # ),
+    # (
+    #     ParseConfig(
+    #         region_name="Тернопільська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622368/",
+    #         save_path="data/ternopil.json"
+    #     ),
+    #     parser
+    # ),
+    # (
+    #     ParseConfig(
+    #         region_name="Вінницька область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622232/",
+    #         save_path="data/vinnytsia.json"
+    #     ),
+    #     parser
+    # ),
+    # (
+    #     ParseConfig(
+    #         region_name="Волинська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622240/",
+    #         save_path="data/volyn.json"
+    #     ),
+    #     parser
+    # ),
+    # (
+    #     ParseConfig(
+    #         region_name="Житомирська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622240/",
+    #         save_path="data/zhytomyr.json"
+    #     ),
+    #     parser
+    # ),
+    # (
+    #     ParseConfig(
+    #         region_name="Закарпатська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622272/",
+    #         save_path="data/zakarpattia.json"
+    #     ),
+    #     parser
+    # ),
+    # (
+    #     ParseConfig(
+    #         region_name="Івано-Франківська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622248/",
+    #         save_path="data/ivano-frankivsk.json"
+    #     ),
+    #     parser
+    # ),
+    # (
+    #     ParseConfig(
+    #         region_name="Київська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622252/",
+    #         save_path="data/kyiv.json"
+    #     ),
+    #     parser
+    # ),
+    # (
+    #     ParseConfig(
+    #         region_name="Кіровоградська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622304/",
+    #         save_path="data/kirovohrad.json"
+    #     ),
+    #     parser
+    # ),
+    # (
+    #     ParseConfig(
+    #         region_name="Львівська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622260/",
+    #         save_path="data/lviv.json"
+    #     ),
+    #     parser
+    # ),
+    # (
+    #     ParseConfig(
+    #         region_name="Миколаївська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622264/",
+    #         save_path="data/mykolaiv.json"
+    #     ),
+    #     parser
+    # ),
+    # # Полтавська область
+    # (
+    #     ParseConfig(
+    #         region_name="Полтавська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622276/",
+    #         save_path="data/poltava.json"
+    #     ),
+    #     parser
+    # ),
+    # # Сумська область
+    # (
+    #     ParseConfig(
+    #         region_name="Сумська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622280/",
+    #         save_path="data/sumy.json"
+    #     ),
+    #     parser
+    # ),
+    # # Харківська область
+    # (
+    #     ParseConfig(
+    #         region_name="Харківська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622284/",
+    #         save_path="data/kharkiv.json"
+    #     ),
+    #     parser
+    # ),
+    # # Херсонська область
+    # (
+    #     ParseConfig(
+    #         region_name="Херсонська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622288/",
+    #         save_path="data/kherson.json"
+    #     ),
+    #     parser
+    # ),
+    # # Хмельницька область
+    # (
+    #     ParseConfig(
+    #         region_name="Хмельницька область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622292/",
+    #         save_path="data/khmelnytskyi.json"
+    #     ),
+    #     parser
+    # ),
+    # # Черкаська область
+    # (
+    #     ParseConfig(
+    #         region_name="Черкаська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622296/",
+    #         save_path="data/cherkasy.json"
+    #     ),
+    #     parser
+    # ),
+    # (
+    #     ParseConfig(
+    #         region_name="Чернівецька область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622300/",
+    #         save_path="data/chernivtsi.json"
+    #     ),
+    #     parser
+    # ),
+    # (
+    #     ParseConfig(
+    #         region_name="Чернігівська область",
+    #         url="https://www.uz.gov.ua/about/activity/electropostachannia/electro_consumers/temporary_shutdown/grafikiobmezhen/622304/",
+    #         save_path="data/chernihiv.json"
+    #     ),
+    #     parser
+    # ),
 ]
 
 asyncio.run(run_periodically(parsers))
--- parser/storage/json_writer.py
+++ parser/storage/json_writer.py
@@ -1,12 +1,13 @@
 import json
 import os
+from datetime import datetime
 
 
 def save_to_json(data, save_path: str):
-    """
-    Зберігає або оновлює дані в JSON файлі.
-    Якщо файл існує, додає нову дату до існуючих даних.
-    """
+
+    # Зберігає або оновлює дані в JSON файлі.
+    # Якщо файл існує, додає нову сьогоднішню або майбутню відому дату до існуючих даних
+
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
 
     existing_data = {}
@@ -17,7 +18,19 @@ def save_to_json(data, save_path: str):
         except (json.JSONDecodeError, FileNotFoundError):
             existing_data = {}
 
-    existing_data.update(data)
+
+    today = datetime.now().date()
+
+    filtered_data = {}
+    for date_str, queues in existing_data.items():
+        try:
+            date_obj = datetime.strptime(date_str, "%d.%m.%Y").date()
+            if date_obj >= today:
+                filtered_data[date_str] = queues
+        except ValueError:
+            continue
+
+    filtered_data.update(data)
 
     with open(save_path, "w", encoding="utf-8") as f:
-        json.dump(existing_data, f, ensure_ascii=False, indent=2)
+        json.dump(filtered_data, f, ensure_ascii=False, indent=2)
